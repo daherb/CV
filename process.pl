5c5cfde2826209b404ad4b857e923ea93c239fc3
Herbert Lange <sonofchaos@gmx.de>
2021-04-20 19:24:10 +0200

    update process.pl to also handle bind tokens

--- process.pl
+++ process.pl
@@ -1,8 +1,12 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
 while(my $line = <>) {
+    # evaluate all code between #( and )#
     $line =~ s/\#\((.*?)\)\#/$1/eeg;
+    # remove all GF bind tokens and surrounding spaces
+    $line =~ s/ {0,1}&\+ {0,1}//g;
     print($line);
     
 }
